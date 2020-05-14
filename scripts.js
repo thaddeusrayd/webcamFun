934692d70dc2e0ae46edf4360b4c36e53d7f74f8
thaddeusrayd <thaddeusrayd@gmail.com>
2020-05-14 14:30:10 -0500

    add and call getVideo func; add paintToCanvas func

--- scripts.js
+++ scripts.js
@@ -3,3 +3,27 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+        .then(localMediaStream => {
+            video.srcObject = localMediaStream; // convert to something our video player understands
+            video.play();
+        })
+        .catch(err => {
+            console.error(`OH NO!!!`, err);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+getVideo();
