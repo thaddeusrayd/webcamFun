6a0ff2d003017ec27fca51575c18930d9b32f10a
thaddeusrayd <thaddeusrayd@gmail.com>
2020-05-14 15:01:20 -0500

    add green screen filter, finishing touches

--- scripts.js
+++ scripts.js
@@ -26,7 +26,8 @@ function paintToCanvas() {
         let pixels = ctx.getImageData(0, 0, width, height); // take pixels out
         // pixels = redEffect(pixels); // fuck around a little bit
 
-        pixels = rgbSplit(pixels);
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.1; // increase to reduce ghost effect
         ctx.putImageData(pixels, 0, 0); // replace
     }, 16);
 }
@@ -57,10 +58,37 @@ function redEffect(pixels) {
 function rgbSplit(pixels) {
     for( let i = 0; i < pixels.data. length; i += 4) {
         pixels.data[i - 150] = pixels.data[i + 0] + 200; // RED
-        pixels.data[i + 100] = pixels.data[i + 1] - 50; // GREEN
-        pixels.data[i - 150] = pixels.data[i + 2] * 0.5; // Blue
+        pixels.data[i + 500] = pixels.data[i + 1] - 50; // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2] * 0.5; // Blue
     } 
     return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0; // 4th pixel is the transparency one
+    }
+  }
+
+  return pixels;
 }
 
 getVideo();
