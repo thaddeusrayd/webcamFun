3b9e70f6fa0e409b7c97a91d139355c1423e2e92
thaddeusrayd <thaddeusrayd@gmail.com>
2020-05-14 14:52:09 -0500

    add red effect filter

--- scripts.js
+++ scripts.js
@@ -23,6 +23,9 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height); // take pixels out
+        pixels = redEffect(pixels); // fuck around a little bit
+        ctx.putImageData(pixels, 0, 0); // replace
     }, 16);
 }
 
@@ -36,8 +39,17 @@ function takePhoto() {
     const link = document.createElement('a');
     link.href = data;
     link.setAttribute('download', 'handsome');
-    link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
     strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for( let i = 0; i < pixels.data. length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
 }
 
 getVideo();
