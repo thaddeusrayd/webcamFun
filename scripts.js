f9b6380001f5483cdae86cc14f9c272d1cf6d2b6
thaddeusrayd <thaddeusrayd@gmail.com>
2020-05-14 14:39:34 -0500

    add takePhoto func and attach paintToCanvas to 'canplay' event listener

--- scripts.js
+++ scripts.js
@@ -26,4 +26,20 @@ function paintToCanvas() {
     }, 16);
 }
 
+function takePhoto() {
+    // play sound
+    snap.currentTime = 0;
+    snap.play();
+
+    // take data out of canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.textContent = 'Download Image';
+    strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
